66e00ca7bf65ae89e227afef4f5b1b0b1eb04690
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-04-07 22:43:02 -0500

    Update Test_producto.py

--- Parcial2Parte1/Test/Test_producto.py
+++ Parcial2Parte1/Test/Test_producto.py
@@ -1,1 +1,20 @@
+import unittest
+from modelo.Producto import Producto
 
+
+class TestProducto(unittest.TestCase):
+    def test_crear_producto_valido(self):
+        producto = Producto("Fertilizante", 20000)
+        self.assertEqual(producto.nombre, "Fertilizante")
+        self.assertEqual(producto.precio, 20000)
+
+    def test_nombre_invalido(self):
+        with self.assertRaises(ValueError):
+            Producto("", 20000)
+
+    def test_precio_invalido(self):
+        with self.assertRaises(ValueError):
+            Producto("Fertilizante", -100)
+
+if __name__ == "__main__":
+    unittest.main()
