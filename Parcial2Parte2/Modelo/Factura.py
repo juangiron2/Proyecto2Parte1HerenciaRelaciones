e857fb870d2de4e00790cd236d77f16697c5ace4
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-05-06 10:16:42 -0500

    Update Factura.py

--- Parcial2Parte2/Modelo/Factura.py
+++ Parcial2Parte2/Modelo/Factura.py
@@ -2,30 +2,46 @@ from datetime import datetime
 
 class Factura:
     def __init__(self, cliente, fecha):
-        self.cliente = cliente
+        self._cliente = cliente  # Atributo privado
         self.set_fecha(fecha)
-        self.productos = []
-        self.total = 0.0
+        self._productos = []
+        self._total = 0.0
+        self._id_factura = None  # Se asignará externamente con uuid
 
     def set_fecha(self, fecha):
         try:
-            self.fecha = datetime.strptime(fecha, "%Y-%m-%d").date()
+            self._fecha = datetime.strptime(fecha, "%Y-%m-%d").date()
         except ValueError:
             raise ValueError("La fecha debe estar en formato YYYY-MM-DD")
 
+    def get_fecha(self):
+        return self._fecha
+
+    def set_id_factura(self, id_factura):
+        self._id_factura = id_factura
+
+    def get_id_factura(self):
+        return self._id_factura
+
     def agregar_producto(self, producto):
-        self.productos.append(producto)
-        self.total += producto.precio
+        self._productos.append(producto)
+        self._total += producto.get_precio()  # Usar método getter
 
     def obtener_detalle_productos(self):
-        return "\n".join([str(producto) for producto in self.productos])
+        return "\n".join([str(producto) for producto in self._productos])
 
     def obtener_total(self):
-        #Método que devuelve el total de la factura.
-        return self.total
+        return self._total
+
+    def get_cliente(self):
+        return self._cliente
+
+    def get_productos(self):
+        return self._productos
 
     def __str__(self):
-        return f"Factura del Cliente: {self.cliente.nombre}\n" \
-               f"Fecha: {self.fecha}\n" \
+        return f"Factura ID: {self._id_factura}\n" \
+               f"Cliente: {self._cliente.get_nombre()}\n" \
+               f"Fecha: {self._fecha}\n" \
                f"Productos:\n{self.obtener_detalle_productos()}\n" \
-               f"Total: ${self.total:.2f}"
+               f"Total: ${self._total:.2f}"
