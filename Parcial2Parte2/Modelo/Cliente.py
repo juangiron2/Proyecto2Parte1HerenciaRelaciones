78d430c0b00da8eb40c937235004557a6cd27650
juangiron2 <143913201+juangiron2@users.noreply.github.com>
2025-05-06 10:14:11 -0500

    Update Cliente.py

--- Parcial2Parte2/Modelo/Cliente.py
+++ Parcial2Parte2/Modelo/Cliente.py
@@ -1,17 +1,29 @@
+# Modelo/Cliente.py
+
 class Cliente:
     def __init__(self, nombre, cedula):
-        self.nombre = nombre
-        self.cedula = cedula
-        self.pedidos = []
+        self.__nombre = nombre
+        self.__cedula = cedula
+        self.__facturas = []
 
-    def agregar_pedido(self, pedido):
-        self.pedidos.append(pedido)
+    # Getters y setters
+    def get_nombre(self):
+        return self.__nombre
 
-    def obtener_total_compras(self):
-        return sum(pedido.obtener_total() for pedido in self.pedidos)
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
 
-    def obtener_numero_pedidos(self):
-        return len(self.pedidos)
+    def get_cedula(self):
+        return self.__cedula
+
+    def set_cedula(self, cedula):
+        self.__cedula = cedula
+
+    def get_facturas(self):
+        return self.__facturas
+
+    def agregar_factura(self, factura):
+        self.__facturas.append(factura)
 
     def __str__(self):
-        return f"Cliente: {self.nombre}, Cédula: {self.cedula}, Número de pedidos: {self.obtener_numero_pedidos()}"
+        return f"Cliente: {self.__nombre}, Cédula: {self.__cedula}"
